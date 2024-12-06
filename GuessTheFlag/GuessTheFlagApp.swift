82c3b4b1aa707c399ebd28724dc57e8bc6c192a6
Ertuğrul Pancar <21220030023@mersin.edu.tr>
2024-12-06 09:34:30 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ertuğrul Pancar on 6.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
